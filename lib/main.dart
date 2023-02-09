975086237c86dd41df98e585ab034c9fd7891808
mac <zayns1914@gmail.com>
2023-02-09 18:07:55 +0200

    refactor

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:mapswithme/directions_repository.dart';
+
+import 'directions_model.dart';
 
 void main() {
   runApp(const MyApp());
@@ -29,11 +32,14 @@ class MapScreen extends StatefulWidget {
 
 class _MapScreenState extends State<MapScreen> {
   final _initialPosition = const CameraPosition(
-      target: LatLng(31.477157832309437, 34.405053122885384), zoom: 11.5);
+      // target: LatLng(31.477157832309437, 34.405053122885384),
+      target: LatLng(37.773972, -122.431297),
+      zoom: 11.5);
 
   GoogleMapController? _googleMapController;
   Marker? _origin;
   Marker? _destination;
+  Directions? _info;
 
   @override
   void dispose() {
@@ -89,48 +95,115 @@ class _MapScreenState extends State<MapScreen> {
       floatingActionButtonLocation: FloatingActionButtonLocation.startFloat,
       floatingActionButton: FloatingActionButton(
         backgroundColor: Theme.of(context).primaryColor,
-        child: const Icon(
-          Icons.center_focus_strong,
-          color: Colors.black,
+        foregroundColor: Colors.black,
+        onPressed: () => _googleMapController!.animateCamera(
+          _info != null
+              ? CameraUpdate.newLatLngBounds(_info!.bounds, 100.0)
+              : CameraUpdate.newCameraPosition(_initialPosition),
         ),
-        onPressed: () => _googleMapController!
-            .animateCamera(CameraUpdate.newCameraPosition(_initialPosition)),
+        child: const Icon(Icons.center_focus_strong),
       ),
-      body: GoogleMap(
-        onMapCreated: (controller) => _googleMapController = controller,
-        initialCameraPosition: _initialPosition,
-        myLocationButtonEnabled: false,
-        myLocationEnabled: false,
-        markers: {
-          if (_origin != null) _origin!,
-          if (_destination != null) _destination!,
-        },
-        onLongPress: _addMarker,
+      // floatingActionButton: FloatingActionButton(
+      //   backgroundColor: Theme.of(context).primaryColor,
+      //   child: const Icon(
+      //     Icons.center_focus_strong,
+      //     color: Colors.black,
+      //   ),
+      //   onPressed: () => _googleMapController!
+      //       .animateCamera(CameraUpdate.newCameraPosition(_initialPosition)),
+      // ),
+      body: Stack(
+        alignment: Alignment.center,
+        children: [
+          GoogleMap(
+            myLocationButtonEnabled: false,
+            zoomControlsEnabled: false,
+            initialCameraPosition: _initialPosition,
+            onMapCreated: (controller) => _googleMapController = controller,
+            markers: {
+              if (_origin != null) _origin!,
+              if (_destination != null) _destination!
+            },
+            polylines: {
+              if (_info != null)
+                Polyline(
+                  polylineId: const PolylineId('overview_polyline'),
+                  color: Colors.red,
+                  width: 5,
+                  points: _info!.polylinePoints
+                      .map((e) => LatLng(e.latitude, e.longitude))
+                      .toList(),
+                ),
+            },
+            onLongPress: _addMarker,
+          ),
+          if (_info != null)
+            Positioned(
+              top: 20.0,
+              child: Container(
+                padding: const EdgeInsets.symmetric(
+                  vertical: 6.0,
+                  horizontal: 12.0,
+                ),
+                decoration: BoxDecoration(
+                  color: Colors.yellowAccent,
+                  borderRadius: BorderRadius.circular(20.0),
+                  boxShadow: const [
+                    BoxShadow(
+                      color: Colors.black26,
+                      offset: Offset(0, 2),
+                      blurRadius: 6.0,
+                    )
+                  ],
+                ),
+                child: Text(
+                  '${_info!.totalDistance}, ${_info!.totalDuration}',
+                  style: const TextStyle(
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ),
+            ),
+        ],
       ),
     );
   }
 
-  _addMarker(LatLng pos) {
+  void _addMarker(LatLng pos) async {
     if (_origin == null || (_origin != null && _destination != null)) {
+      // Origin is not set OR Origin/Destination are both set
+      // Set origin
       setState(() {
         _origin = Marker(
-            markerId: const MarkerId("origin"),
-            infoWindow: const InfoWindow(title: "Origin"),
-            icon: BitmapDescriptor.defaultMarkerWithHue(
-                BitmapDescriptor.hueGreen),
-            position: pos);
-
+          markerId: const MarkerId('origin'),
+          infoWindow: const InfoWindow(title: 'Origin'),
+          icon:
+              BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueGreen),
+          position: pos,
+        );
+        // Reset destination
         _destination = null;
+
+        // Reset info
+        _info = null;
       });
     } else {
+      // Origin is already set
+      // Set destination
       setState(() {
         _destination = Marker(
-            markerId: const MarkerId("destination"),
-            infoWindow: const InfoWindow(title: "Destination"),
-            icon:
-                BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueBlue),
-            position: pos);
+          markerId: const MarkerId('destination'),
+          infoWindow: const InfoWindow(title: 'Destination'),
+          icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueBlue),
+          position: pos,
+        );
       });
+
+      // Get directions
+      final directions = await DirectionRepo()
+          .getDirections(origin: _origin!.position, destination: pos);
+      setState(() => _info = directions);
     }
   }
 }
